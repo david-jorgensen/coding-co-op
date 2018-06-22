206155a7ed715651cd8a0c3759f401e7be6ceb90
davidjorgensen2002 <40479261+davidjorgensen2002@users.noreply.github.com>
2018-06-22 10:05:23 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string answer = "green";
+    if (answer=="pink"){
+        cout << "You are a loser"
+    } else {
+        cout << "Whew, you dodged a bullet"
+    };
+    return 0;
+};
